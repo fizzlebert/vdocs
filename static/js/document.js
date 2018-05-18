1955da241f4f0e7694914583cb96982ad45bc393
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2018-05-18 15:14:44 +1000

    Use JQuery for detecting keypresses

--- static/js/document.js
+++ static/js/document.js
@@ -1,17 +1,6 @@
-function vim(keyboard){
-	var keynum;
-
-    if(keyboard.which){
-	    keynum = keyboard.which;
-		key = String.fromCharCode(keynum);
-	}
-	console.log(key)
-
-	if(key === "x"){
-		console.log(document.getElementById("text").innerHTML)
-		console.log("it sss");
-	}
-    $( "#text" ).keypress(function() {
-	console.log("kjkj")
+$(document).ready(function(){
+    $("#text").keypress(function(){
+		alert("test");
+        console.log(e.key);
     });
-}
+});
